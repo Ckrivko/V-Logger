13fc54229410e2338d9ca1b65d0480a41892b0d1
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2021-02-03 17:58:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace V_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
